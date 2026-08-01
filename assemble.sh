72adcf356d634f21ca6c998c9bed6a51a9b0d565
Mohamed Zaidane <ali.zaidane.2007@gmail.com>
2026-08-01 17:46:01 +0100

    assemble.sh: gunzip pipeline; site chunk 00/12

--- assemble.sh
+++ assemble.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# Rebuild the single-file site from the base64 chunks in chunks/.
 set -e
 mkdir -p dist
-cat chunks/part.* | base64 -d > dist/index.html
+cat gz/part.* | base64 -d | gzip -d > dist/index.html
 echo "assembled dist/index.html"
